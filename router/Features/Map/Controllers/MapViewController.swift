4abc925e2f7cf80341eb79b45be9828e211287e7
johnson ejezie <johnsonejezie@gmail.com>
2019-05-11 16:36:52 +0100

    set up view controller and view model for airport search

---
+++ router/Features/Map/Controllers/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  router
+//
+//  Created by Johnson Ejezie on 11/05/2019.
+//  Copyright © 2019 Guestlogix. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ router/Features/Map/Controllers/RouteCollectionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RouteCollectionViewController.swift
+//  router
+//
+//  Created by Johnson Ejezie on 11/05/2019.
+//  Copyright © 2019 Guestlogix. All rights reserved.
+//
+
+import UIKit
+
+class RouteCollectionViewController: UIViewController {
+
+    @IBOutlet var contentView: UIView!
+    @IBOutlet var arrivalTextField: UITextField!
+    @IBOutlet var departureTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
