66a76f1109a37af206db105491907c76d0ea7c0c
johnson ejezie <johnsonejezie@gmail.com>
2019-05-11 10:17:38 +0100

    create route model class

---
+++ router/Models/Route.swift
@@ -0,0 +1,41 @@
+//
+//  Route.swift
+//  router
+//
+//  Created by Johnson Ejezie on 11/05/2019.
+//  Copyright © 2019 Guestlogix. All rights reserved.
+//
+
+import Foundation
+
+class Route: Equatable {
+    static func == (lhs: Route, rhs: Route) -> Bool {
+        return lhs.airlineID == rhs.airlineID && lhs.origin == rhs.origin && lhs.destination == rhs.destination
+    }
+
+    let airlineID: String
+    let origin: String
+    let destination: String
+
+    init(airlineID: String, origin: String, destination: String) {
+        self.airlineID = airlineID
+        self.origin = origin
+        self.destination = destination
+    }
+
+    static var routes: [Route] = []
+
+    static func parseCSV(_ completion: @escaping (() -> Void)) {
+        let path = Bundle.main.path(forResource: "routes", ofType: "csv")!
+        let routeImporter = CSVImporter<Route>(path: path)
+        routeImporter.startImportingRecords(structure: { (headerValues) in
+            print(headerValues)
+        }) { (recordValues) -> Route in
+            return Route(airlineID: recordValues["Airline Id"]!, origin: recordValues["Origin"]!, destination: recordValues["Destination"]!)
+            }.onFinish { (importedRecords) -> Void in
+                routes = importedRecords
+                completion()
+        }
+    }
+
+}
