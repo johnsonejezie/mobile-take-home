b712a23b2a2b655c0e66ed7c9bffaa9627b0ba38
johnson ejezie <johnsonejezie@gmail.com>
2019-05-11 10:17:08 +0100

    test parsing of Route class

---
+++ routerTests/CSVParsingTests.swift
@@ -0,0 +1,38 @@
+//
+//  CSVParsingTests.swift
+//  routerTests
+//
+//  Created by Johnson Ejezie on 11/05/2019.
+//  Copyright © 2019 Guestlogix. All rights reserved.
+//
+
+import XCTest
+@testable import router
+
+class CSVParsingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testParsingRoutesCSVToRoutesClass() {
+        let expectation = self.expectation(description: "Parsing Routes CSV")
+        Route.parseCSV {
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+        XCTAssertTrue(!Route.routes.isEmpty, "routes is not empty")
+        let route = Route.routes.first
+        XCTAssert(route != nil)
+        XCTAssert(route?.airlineID != nil)
+        XCTAssert(route?.origin != nil)
+        XCTAssert(route?.destination != nil)
+    }
+
+
+
+}
