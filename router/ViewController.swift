1c9817141031e7a3ae6e3c892ab785eb87997fbd
johnson ejezie <johnsonejezie@gmail.com>
2019-05-11 09:01:57 +0100

    project set up

---
+++ router/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  router
+//
+//  Created by Johnson Ejezie on 11/05/2019.
+//  Copyright © 2019 Guestlogix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
