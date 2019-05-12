3f0c2db4fb38fa7937a6f779a45e7e38fce9bde1
johnson ejezie <johnsonejezie@gmail.com>
2019-05-12 16:15:50 +0100

    update test to use mock data

--- routerTests/RouteCollectionViewModelTests.swift
+++ routerTests/RouteCollectionViewModelTests.swift
@@ -12,9 +12,14 @@ import XCTest
 class RouteCollectionViewModelTests: XCTestCase {
 
     var viewModel: RouteCollectionViewModel!
+    let airports = [
+        Airport(name: "Queen Alia International Airport", city: "Amman", iata: "AMM", latitude: "31.72260094", longitude: "35.99319839", country: "Jordan"),
+        Airport(name: "Krasnodar Pashkovsky International Airport", city: "Krasnodar", iata: "KRR", latitude: "45.03469849", longitude: "39.17050171", country: "Russia"),
+        Airport(name: "Shimojishima Airport", city: "Shimojishima", iata: "SHI", latitude: "24.82670021", longitude: "125.1449966", country: "Japan")
+    ]
 
     override func setUp() {
-        viewModel = RouteCollectionViewModel()
+        viewModel = RouteCollectionViewModel(allAirport: airports)
     }
 
     override func tearDown() {}
@@ -30,45 +35,28 @@ class RouteCollectionViewModelTests: XCTestCase {
     }
 
     func testSearchAirportReturnsDataGivenExistingCity() {
-        let expectation = self.expectation(description: "Search city")
-        let city = "Hofn"
-        viewModel.inputs.search(text: city) {
-            expectation.fulfill()
-        }
-        waitForExpectations(timeout: 15, handler: nil)
+        let iata = "SHI"
+        viewModel.inputs.search(text: iata)
         XCTAssertFalse(viewModel.outputs.airports.value.isEmpty)
     }
 
     func testSearchAirportReturnsEmptyForCityNotInCSV() {
-        let expectation = self.expectation(description: "Search city")
-        let city = "qqqq"
-        viewModel.inputs.search(text: city) {
-            expectation.fulfill()
-        }
-        waitForExpectations(timeout: 15, handler: nil)
+        let iata = "qqqq"
+        viewModel.inputs.search(text: iata)
         XCTAssertTrue(viewModel.outputs.airports.value.isEmpty)
     }
 
     func testSearchIsAccurate() {
-        let expectation = self.expectation(description: "Search city")
-        let city = "Hofn"
-        viewModel.inputs.search(text: city) {
-            expectation.fulfill()
-        }
-        waitForExpectations(timeout: 15, handler: nil)
+        let iata = "SHI"
+        viewModel.inputs.search(text: iata)
         guard let airport = viewModel.outputs.airports.value.first else {
             XCTFail()
             return
         }
-        XCTAssertTrue(airport.city.lowercased().contains(city.lowercased()))
+        XCTAssertTrue(airport.city.lowercased().contains(iata.lowercased()))
     }
 
     func testSettingAirport() {
-        let expectation = self.expectation(description: "Fetching Airports")
-        viewModel.getAllAirports {
-            expectation.fulfill()
-        }
-        waitForExpectations(timeout: 15, handler: nil)
         viewModel.editingTextField = .arrival
         let airport = Airport(name: "Krasnodar Pashkovsky International Airport", city: "Krasnodar", iata: "KRR", latitude: "45.03469849", longitude: "39.17050171", country: "Russia")
         XCTAssertNotEqual(viewModel.outputs.arrivalAirport, airport)
